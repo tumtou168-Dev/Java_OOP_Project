e362f031c1d92cb524083e9a877d6b0b6a753afe
tumtou <tumtou96@gmail.com>
2025-09-02 08:41:58 +0700

    This is day 2 with Java

---
+++ src/Week1/PrintVaraible.java
@@ -0,0 +1,18 @@
+package Week1;
+
+public class PrintVaraible {
+    public static void main(String[] args) {
+        String firstName = "John";
+        String lastName = "Doe";
+        String address = "Phnom Penh";
+        int id = 20004;
+        int age = 20;
+        String fullName = firstName + " " + lastName;
+
+        System.out.println("Full Name is: " + fullName);
+        System.out.println("Age: " + age);
+        System.out.println("Address: " + address);
+        System.out.println("Id: " + id);
+    }
+
+}
